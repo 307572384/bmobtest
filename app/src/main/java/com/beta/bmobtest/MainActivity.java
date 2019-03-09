15e86716bea2022fcd2264bc11557aa1fb45f2b6
Kevien <307572384@qq.com>
2019-03-09 12:37:43 +0800

    Bmob实现短信登陆验证自动记录密码安卓6.0动态获取权限1.0版本

--- app/src/main/java/com/beta/bmobtest/MainActivity.java
+++ app/src/main/java/com/beta/bmobtest/MainActivity.java
@@ -1,10 +1,13 @@
 package com.beta.bmobtest;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,18 +17,20 @@ import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends AppCompatActivity {
-	private Person   p2;
-	private TextView lgUser;
-	private TextView lgPassword;
-	private Button   btn_ok;
-	private Button   btn_rg;
-
+	private Person                   p2;
+	private TextView                 lgUser;
+	private TextView                 lgPassword;
+	private Button                   btn_ok;
+	private Button                   btn_rg;
+	private CheckBox                 rememberPass;
+	private SharedPreferences        pref;
+	private SharedPreferences.Editor editor;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		Bmob.initialize(this, "你自己的appid");
+		Bmob.initialize(this, "16e74751f4ede2e59f1fcae1e508cc3b");
 
 
 		addControl();
@@ -45,8 +50,18 @@ public class MainActivity extends AppCompatActivity {
 		btn_ok.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+
 				String lgU = lgUser.getText().toString().trim();
 				String lgp = lgPassword.getText().toString().trim();
+				editor=pref.edit();
+				if(rememberPass.isChecked()){
+					editor.putBoolean("remember_password",true);
+					editor.putString("account",lgU);
+					editor.putString("password",lgp);
+				}else {
+					editor.clear();
+				}
+				editor.apply();
 				final BmobUser bu2 = new BmobUser();
 				bu2.setUsername(lgU);
 				bu2.setPassword(lgp);
@@ -70,13 +85,25 @@ public class MainActivity extends AppCompatActivity {
 
 
 		});
+
 	}
 
 	private void addControl() {
-
+		pref= PreferenceManager.getDefaultSharedPreferences(this);
+		rememberPass=(CheckBox)findViewById(R.id.remember);
 		lgUser = (TextView) findViewById(R.id.id_username);
 		lgPassword = (TextView) findViewById(R.id.id_userpassword);
 		btn_ok = (Button) findViewById(R.id.id_ok);
 		btn_rg = (Button) findViewById(R.id.id_register);
+		boolean isRemenber=pref.getBoolean("remember_password",false);
+		if(isRemenber){
+			//将账号和密码都设置到文本中
+			String account=pref.getString("account","");
+			String password=pref.getString("password","");
+			lgUser.setText(account);
+			lgPassword.setText(password);
+			rememberPass.setChecked(true);
+
+		}
 	}
 }
--- app/src/main/java/com/beta/bmobtest/RegisterActivity.java
+++ app/src/main/java/com/beta/bmobtest/RegisterActivity.java
@@ -1,117 +1,251 @@
 package com.beta.bmobtest;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import cn.bmob.v3.BmobSMS;
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.SaveListener;
+import me.weyye.hipermission.HiPermission;
+import me.weyye.hipermission.PermissionCallback;
+import me.weyye.hipermission.PermissionItem;
 
 /**
  * Created by Kevein on 2019/3/1.8:03
  */
 
-public class RegisterActivity extends Activity {
-	private TextView register_user;
-	private TextView register_password;
-	private Button   register_ok;
+public class RegisterActivity extends Activity  implements View.OnClickListener{
+	public static final String TAG = "RegisterActivity";
+	private Intent   mIntent;
+	private Context  mContext;
+	private EditText register_phone;
+	private EditText register_password;
+	private Button   register_ok, register_countdown;
+	private EditText register_ValiDation;//输入验证码
+
+	private void showToast(String text) {//简化Toast
+		Toast.makeText(mContext, text, Toast.LENGTH_SHORT).show();
+	}
+
+	//倒计时广播
+	private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			final String action = intent.getAction();
+			switch (action) {
+				case RegisterCodeTimerService.IN_RUNNING:
+					if (register_countdown.isEnabled())
+						register_countdown.setEnabled(false);
+					//正在倒计时
+					register_countdown.setText("获取验证码(" + intent.getStringExtra("time") + ")");
+					Log.e(TAG, "倒计时中(" + intent.getStringExtra("time") + ")");
+					break;
+				case RegisterCodeTimerService.END_RUNNING:
+					//完成倒计时
+					register_countdown.setEnabled(true);
+					register_countdown.setText("获取验证码");
+					break;
+			}
+		}
+	};
+	// 注册广播
+	private static IntentFilter updateIntentFilter() {
+		final IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(RegisterCodeTimerService.IN_RUNNING);
+		intentFilter.addAction(RegisterCodeTimerService.END_RUNNING);
+		return intentFilter;
+	}
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register_layout);
+		mContext = this;
 		addControl();//加载控件
-		addRegisterShow();//注册方法
-		Bmob.initialize(this, "你自己的appid");
+		Bmob.initialize(this, "16e74751f4ede2e59f1fcae1e508cc3b");
 	}
 
-	private void addRegisterShow() {
-		register_ok.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				String mobile = register_user.getText().toString().trim();
-				String password = register_password.getText().toString().trim();
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// 注册广播
+		registerReceiver(mBroadcastReceiver, updateIntentFilter());
+	}
 
-				//判断是否输入是否是电话号码
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// 移除注册
+		unregisterReceiver(mBroadcastReceiver);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.e(TAG, "onDestroy 方法调用");
+	}
+
+	private boolean isPhoneNumber(String phoneStr) {
+		//定义电话格式的正则表达式
+		String regex = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$";
+		//设定查看模式
+		Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+		//判断Str是否匹配，返回匹配结果
+		Matcher m = p.matcher(phoneStr);
+		return m.matches();
+	}
+
+	private boolean PasswordJudge(String pwStr) {
+
+		//密码判断以字母开头，长度在6~18之间，只能包含字母、数字和下划线
+		String pattern = "^[a-zA-Z]\\w{5,17}$";
+
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(pwStr);
+		return m.find();
+	}
+
+	private void addControl() {
+
+		register_ok = (Button) findViewById(R.id.id_register_ok);
+		register_phone = (EditText) findViewById(R.id.register_phone);
+		register_password = (EditText) findViewById(R.id.register_password);
+		register_countdown = (Button) findViewById(R.id.btn_countdown);
+		register_ValiDation = (EditText) findViewById(R.id.et_validation);
+		register_countdown.setOnClickListener(this);
+		register_ok.setOnClickListener(this);
+
+	}
+
+
+	//动态获取手机状态和发送短信权限
+	private void Permissionx() {
+		List<PermissionItem> permissions = new ArrayList<PermissionItem>();
+		permissions.add(new PermissionItem(Manifest.permission.SEND_SMS, getString(R.string.permission_send_sms_phone), R.drawable.permission_ic_sms));
+		permissions.add(new PermissionItem(Manifest.permission.READ_PHONE_STATE, getString(R.string.permission_read_phone_state), R.drawable.permission_ic_phone));
+		HiPermission.create(mContext)
+				.title(getString(R.string.permission_cus_title))
+				.permissions(permissions)
+				.msg(getString(R.string.permission_cus_msg))
+				.animStyle(R.style.PermissionAnimModal)
+				.style(R.style.PermissionDefaultGreenStyle)
+				.checkMutiPermission(new PermissionCallback() {
+					@Override
+					public void onClose() {
+						Log.i(TAG, "onClose");
+						showToast(getString(R.string.permission_on_close));
+					}
+
+					@Override
+					public void onFinish() {
+						showToast(getString(R.string.permission_completed));
+					}
+
+					@Override
+					public void onDeny(String permission, int position) {
+						Log.i(TAG, "onDeny");
+					}
+
+					@Override
+					public void onGuarantee(String permission, int position) {
+						Log.i(TAG, "onGuarantee");
+					}
+				});
+
+
+	}
+
+	@Override
+	public void onClick(View view) {
+	    String mobile = register_phone.getText().toString().trim();
+//		String password = register_password.getText().toString().trim();
+			switch (view.getId()) {
+
+			case R.id.btn_countdown: {//发送短信验证
+				mIntent = new Intent(mContext, RegisterCodeTimerService.class);//接收倒计时广播
+				Permissionx();//安卓6.0动态获取权限
+				// 将按钮设置为不可用状态
+				register_countdown.setEnabled(false);
+				// 启动倒计时的服务
+				startService(mIntent);
+				//				 通过requestSMSCode方式给绑定手机号的该用户发送指定短信模板的短信验证码
+				BmobSMS.requestSMSCode(mobile, "1", new QueryListener<Integer>() {
+					@Override
+					public void done(Integer smsId, BmobException e) {
+						if (e == null) {
+							showToast(getString(R.string.smssuccess) + smsId);
+						} else {
+							showToast(getString(R.string.failed) + smsId);
+						}
+					}
+				});
+				break;
+			}
+			case R.id.id_register_ok:
+				String password = register_password.getText().toString().trim();//获取密码
+				String valiation = register_ValiDation.getText().toString();//获取验证码
 				boolean phone = isPhoneNumber(mobile);
 				//判断密码
 				boolean pwd = PasswordJudge(password);
 
-				if(mobile.isEmpty() || password.isEmpty())
-				{
-					Toast.makeText(RegisterActivity.this,"用户名或密码不能为空",Toast.LENGTH_SHORT).show();
+				if (mobile.isEmpty()||password.isEmpty()) {
+					showToast(getString(R.string.user_or_password_empty));
+					return;
 				}
 				else if(!phone)
 				{
-					Toast.makeText(RegisterActivity.this,"请输入正确的手机号码",Toast.LENGTH_SHORT).show();
-
+					showToast(getString(R.string.phone_failed));
+					return;
 				}
-				else if(!pwd)
+				else if (!pwd) {
+					showToast(getString(R.string.userpassword_failed));
+					return;
+				} else if(TextUtils.isEmpty(valiation)){
+					showToast(getString(R.string.valiation_empty));
+					return;
+
+				}else
 				{
-					Toast.makeText(RegisterActivity.this,"密码在以字母开头6-18位之前只能包含字母数字和下划线",Toast.LENGTH_SHORT).show();
+					//Bmob注册验证方法
+					User p2 = new User();
+					p2.setMobilePhoneNumber(mobile);
+					p2.setPassword(password);
+					p2.signOrLogin(valiation,new SaveListener<BmobUser>() {//valiation关于获取到验证码
+						@Override
+						public void done(BmobUser bmobUser, BmobException e) {
+							if (e == null) {
+								//判断是否注册成功成功则跳转到登陆的页面
+								Intent intent_register = new Intent(RegisterActivity.this, MainActivity.class);
+								startActivity(intent_register);
+								showToast(getString(R.string.user_success));
+							} else {
+								showToast(getString(R.string.user_failed)+e.getErrorCode()+e.getMessage());
+							}
+						}
+					});
+					break;
 				}
-				else{
-					Bmoblogin();
-				}
 
-			}
-		});
-	}
-private boolean isPhoneNumber(String phoneStr)
-{
-	//定义电话格式的正则表达式
-	String regex = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$";
-	//设定查看模式
-	Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-	//判断Str是否匹配，返回匹配结果
-	Matcher m = p.matcher(phoneStr);
-	return m.matches();
-}
-private boolean PasswordJudge(String pwStr)
-{
-
-	//密码判断以字母开头，长度在6~18之间，只能包含字母、数字和下划线
-	String pattern = "^[a-zA-Z]\\w{5,17}$";
-
-	Pattern r = Pattern.compile(pattern);
-	Matcher m = r.matcher(pwStr);
-	return m.find();
-}
-
-	private void addControl() {
-		register_user = (TextView) findViewById(R.id.id_register_username);
-		register_password = (TextView) findViewById(R.id.id_register_userpassword);
-		register_ok = (Button) findViewById(R.id.id_register_ok);
-
-
-	}
-	private void Bmoblogin()//Bmo注册写入方法
-	{
-		final BmobUser p2 = new BmobUser();
-		p2.setUsername(register_user.getText().toString());
-		p2.setPassword(register_password.getText().toString());
-		p2.signUp(new SaveListener<BmobUser>() {
-			@Override
-			public void done(BmobUser bmobUser, BmobException e) {
-				if (e == null) {
-					//判断是否注册成功成功则跳转到登陆的页面
-					Intent intent_register = new Intent(RegisterActivity.this,MainActivity.class);
-					startActivity(intent_register);
-					Toast.makeText(RegisterActivity.this, "添加数据成功，返回objectId为：" + p2.getObjectId(), Toast.LENGTH_SHORT).show();
-				} else {
-					Toast.makeText(RegisterActivity.this, "用户名或者密码不能为空", Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
+		}
 	}
 }
